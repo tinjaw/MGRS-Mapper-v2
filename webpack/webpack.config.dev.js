feb64b7acbf8b92519a3bb7ed4a797fe519c43f2
James Pistell <jamespistell@gmail.com>
2020-03-01 05:03:09 -0500

    Added webpack bundle analyzer

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,24 +1,26 @@
 const Path = require('path');
 const Webpack = require('webpack');
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const common = require('./webpack.common.js');
+
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'cheap-eval-source-map',
   output: {
-    chunkFilename: 'js/[name].chunk.js'
+    chunkFilename: 'js/[name].chunk.js',
   },
   devServer: {
     inline: true,
-    hot: true
+    hot: true,
   },
   plugins: [
     new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      'process.env.NODE_ENV': JSON.stringify('development'),
     }),
-
+    new BundleAnalyzerPlugin(),
   ],
   module: {
     rules: [
@@ -28,17 +30,17 @@ module.exports = merge(common, {
         enforce: 'pre',
         loader: 'eslint-loader',
         options: {
-          emitWarning: true
-        }
+          emitWarning: true,
+        },
       },
       {
         test: /\.js$/,
         include: Path.resolve(__dirname, '../src'),
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.s?css$/i,
-        //use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
+        // use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
         use: [
           'style-loader',
           'css-loader?sourceMap=true',
@@ -47,11 +49,11 @@ module.exports = merge(common, {
             options: {
               implementation: require('sass'),
               sassOptions: {
-                includePaths: ['./node_modules']
+                includePaths: ['./node_modules'],
               },
-            }
-          }]
-      }
-    ]
-  }
+            },
+          }],
+      },
+    ],
+  },
 });
