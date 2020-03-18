49aac8af50577ff0ffd61c531b5a21d09f919251
James Pistell <jamespistell@gmail.com>
2020-03-18 07:28:55 -0400

    Finished Command Post symbols

--- src/scripts/symbolObjects/commandPost.js
+++ src/scripts/symbolObjects/commandPost.js
@@ -255,124 +255,68 @@ const commandPostObject = {
   'Tactical Command Post': {
     type: 'Command Post',
     affiliation: {
-      friendly: {
-        path_1: {
-          d: 'M25,150 L25,240',
-        },
-        text_1: {
-          symbolText: 'TAC',
-          x: '100',
-          y: '172',
-          fontSize: '22',
-          textAnchor: 'middle',
-        },
+      get friendly() {
+        const propertyToModify = JSON.parse(JSON.stringify(friendlyStaff));
+        propertyToModify.text_1.symbolText = 'TAC';
+        return propertyToModify;
       },
       get friendlyTemplated() {
         return this.friendly;
       },
-      hostile: {
-        path_1: {
-          d: 'M28,100 L28,240',
-        },
-        text_1: {
-          symbolText: 'TAC',
-          x: '100',
-          y: '192',
-          fontSize: '22',
-          textAnchor: 'middle',
-        },
+      get hostile() {
+        const propertyToModify = JSON.parse(JSON.stringify(hostileStaff));
+        propertyToModify.text_1.symbolText = 'TAC';
+        return propertyToModify;
       },
       get hostileTemplated() {
         return this.hostile;
       },
-      unknown: {
-        path_1: {
-          d: 'M30.75,100 L30.75,240',
-        },
-        text_1: {
-          symbolText: 'TAC',
-          x: '100',
-          y: '192',
-          fontSize: '22',
-          textAnchor: 'middle',
-        },
+      get unknown() {
+        const propertyToModify = JSON.parse(JSON.stringify(unknownStaff));
+        propertyToModify.text_1.symbolText = 'TAC';
+        return propertyToModify;
       },
       get pending() {
         return this.unknown;
       },
-      neutral: {
-        path_1: {
-          d: 'M45,155 L45,240',
-        },
-        text_1: {
-          symbolText: 'TAC',
-          x: '100',
-          y: '180',
-          fontSize: '22',
-          textAnchor: 'middle',
-        },
+      get neutral() {
+        const propertyToModify = JSON.parse(JSON.stringify(neutralStaff));
+        propertyToModify.text_1.symbolText = 'TAC';
+        return propertyToModify;
       },
     },
   },
   'Main Command Post': {
     type: 'Command Post',
     affiliation: {
-      friendly: {
-        path_1: {
-          d: 'M25,150 L25,240',
-        },
-        text_1: {
-          symbolText: 'MAIN',
-          x: '100',
-          y: '172',
-          fontSize: '22',
-          textAnchor: 'middle',
-        },
+      get friendly() {
+        const propertyToModify = JSON.parse(JSON.stringify(friendlyStaff));
+        propertyToModify.text_1.symbolText = 'MAIN';
+        return propertyToModify;
       },
       get friendlyTemplated() {
         return this.friendly;
       },
-      hostile: {
-        path_1: {
-          d: 'M28,100 L28,240',
-        },
-        text_1: {
-          symbolText: 'MAIN',
-          x: '100',
-          y: '192',
-          fontSize: '22',
-          textAnchor: 'middle',
-        },
+      get hostile() {
+        const propertyToModify = JSON.parse(JSON.stringify(hostileStaff));
+        propertyToModify.text_1.symbolText = 'MAIN';
+        return propertyToModify;
       },
       get hostileTemplated() {
         return this.hostile;
       },
-      unknown: {
-        path_1: {
-          d: 'M30.75,100 L30.75,240',
-        },
-        text_1: {
-          symbolText: 'MAIN',
-          x: '100',
-          y: '192',
-          fontSize: '22',
-          textAnchor: 'middle',
-        },
+      get unknown() {
+        const propertyToModify = JSON.parse(JSON.stringify(unknownStaff));
+        propertyToModify.text_1.symbolText = 'MAIN';
+        return propertyToModify;
       },
       get pending() {
         return this.unknown;
       },
-      neutral: {
-        path_1: {
-          d: 'M45,155 L45,240',
-        },
-        text_1: {
-          symbolText: 'MAIN',
-          x: '100',
-          y: '180',
-          fontSize: '22',
-          textAnchor: 'middle',
-        },
+      get neutral() {
+        const propertyToModify = JSON.parse(JSON.stringify(neutralStaff));
+        propertyToModify.text_1.symbolText = 'MAIN';
+        return propertyToModify;
       },
     },
   },
